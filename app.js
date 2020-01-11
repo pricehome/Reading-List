406ae9b752c7f1d1dfe9407bf29e50cb08568c21
Mark Price <pricehome@gmail.com>
2020-01-11 12:18:51 -0600

    es6

--- app.js
+++ app.js
@@ -1,61 +1,61 @@
 // Book Constructor
-function Book(title, author, isbn) {
-	this.title = title
-	this.author = author
-	this.isbn = isbn
+class Book {
+	constructor(title, author, isbn) {
+		this.title = title
+		this.author = author
+		this.isbn = isbn
+	}
 }
 
 // UI Constructor
-function UI() {}
-// add book to list
-UI.prototype.addBookToList = function(book) {
-	const list = document.getElementById('book-list')
-	// create tr element
-	const row = document.createElement('tr')
-	// insert cols
-	row.innerHTML = `
+class UI {
+	constructor() {}
+	// add book to list
+	addBookToList(book) {
+		const list = document.getElementById('book-list')
+		// create tr element
+		const row = document.createElement('tr')
+		// insert cols
+		row.innerHTML = `
   <td>${book.title}</td>
   <td>${book.author}</td>
   <td>${book.isbn}</td>
   <td><a href="#" class="delete">X<a></td>`
-
-	list.appendChild(row)
-}
-
-// Show alert
-UI.prototype.showAlert = function(message, className) {
-	// create div
-	const div = document.createElement('div')
-	// add classes
-	div.className = `alert ${className}`
-
-	// add text
-	div.appendChild(document.createTextNode(message))
-	// get parent
-	const container = document.querySelector('.container')
-	// get form
-	const form = document.querySelector('book-form')
-	// insert alert
-	container.insertBefore(div, form)
-
-	// Timeout after 3 sec
-	setTimeout(function() {
-		document.querySelector('.alert').remove()
-	}, 3000)
-}
-
-// delete book
-UI.prototype.deleteBook = function(target) {
-	if (target.className === 'delete') {
-		target.parentElement.parentElement.remove()
+		list.appendChild(row)
+	}
+	// Show alert
+	showAlert(message, className) {
+		// create div
+		const div = document.createElement('div')
+		// add classes
+		div.className = `alert ${className}`
+		// add text
+		div.appendChild(document.createTextNode(message))
+		// get parent
+		const container = document.querySelector('.container')
+		// get form
+		const form = document.querySelector('book-form')
+		// insert alert
+		container.insertBefore(div, form)
+		// Timeout after 3 sec
+		setTimeout(function() {
+			document.querySelector('.alert').remove()
+		}, 3000)
+	}
+	// delete book
+	deleteBook(target) {
+		if (target.className === 'delete') {
+			target.parentElement.parentElement.remove()
+		}
+	}
+	// clear fields
+	clearFields() {
+		document.getElementById('title').value = ''
+		document.getElementById('author').value = ''
+		document.getElementById('isbn').value = ''
 	}
 }
-// clear fields
-UI.prototype.clearFields = function() {
-	document.getElementById('title').value = ''
-	document.getElementById('author').value = ''
-	document.getElementById('isbn').value = ''
-}
+
 // Event Listener for add book
 document.getElementById('book-form').addEventListener('submit', function(e) {
 	// Get form values
